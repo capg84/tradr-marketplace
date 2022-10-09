a0783195eef450034bf88606d46a19dde39f9cf4
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-10-10 00:45:45 +0100

    modified index.js and added more structure

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
+const paymentRoutes = require('./paymentRoutes');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
-router.use('/products', productRoutes);
+router.use('/payment', paymentRoutes);
 router.use('/api', apiRoutes);
 
 

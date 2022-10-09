74e65b08dc074b5b33aa22b94a326617827fdb76
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-10-10 00:26:31 +0100

    modified index.js routes

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/products', productRoutes);
+router.use('/api', apiRoutes);
+
+
+module.exports = router;

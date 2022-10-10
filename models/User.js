df20f761a5dce9056618ad974740ce7f5981d552
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-10-10 01:37:00 +0100

    added checkpassword function to user model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -15,7 +20,7 @@ User.init(
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-            isEmail: true 
+          isEmail: true 
         }
     },
     password: {

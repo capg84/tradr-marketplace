93c6d894b72e51b28685451d17b5f9a703c87444
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-10-10 01:02:38 +0100

    corrected category_id foreign key

--- models/Product.js
+++ models/Product.js
@@ -35,7 +35,7 @@ Product.init(
             type: DataTypes.INTEGER,
             allowNull: false,
             references: {
-              model: 'product',
+              model: 'category',
               key: 'id',
             },
           },

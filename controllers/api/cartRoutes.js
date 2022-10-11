da39263845e3b439c5d56d4237cc503714039887
DMon <davemonaghan@gmx.com>
2022-10-11 17:31:35 +0100

    Add to cart and delete from wishlist functions added for front and back end

--- controllers/api/cartRoutes.js
+++ controllers/api/cartRoutes.js
@@ -1,1 +1,44 @@
+const router = require('express').Router();
+const { Cart } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+// Adds item to Cart
+router.post('/add', withAuth, async (req, res) => {
+
+    try {
+        const cart = await Cart({
+            product_id: req.body.id,
+            user_id: req.session.user_id,
+            quantity: req.body.quantity
+          
+        });
+
+        res.json(cart)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+
+// Deletes item from Cart
+router.delete('/delete/:id', withAuth, async (req, res) => {
+
+    try {
+        const cart = await Cart.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            },
+        });
+        if (!cart) {
+            res.status(404).json({ message: 'No product found' });
+            return;
+        }
+        res.status(200).json(cart);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,1 +1,1 @@
-module.exports = router;
+// module.exports = router;

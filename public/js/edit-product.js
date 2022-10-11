3aada4bdad83d0ccd5873449e939d41eb64a51ae
P Luong <p-luong@live.co.uk>
2022-10-11 21:43:55 +0100

    amended edit listing function

--- public/js/edit-product.js
+++ public/js/edit-product.js
@@ -1,28 +1,29 @@
 const editListingHandler = async (event) => {
-    console.log("Edit button clicked");
+    console.log("Update button clicked");
     event.preventDefault();
 
-    const first_name = document.querySelector('#inputFName').value.trim();
-    const last_name = document.querySelector('#inputLName').value.trim();
-    const email = document.querySelector('#inputEmail2').value.trim();
-    const password = document.querySelector('#inputPassword2').value.trim();
-  
-    if (first_name && last_name && email && password) {
-        console.log(first_name + last_name + email + password);
-        const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ email, password, first_name, last_name }),
+    const product_name = document.querySelector('#product_name').value.trim();
+    const description = document.querySelector('#description').value.trim();
+    const price = document.querySelector('#product_price').value.trim();
+    const image = document.querySelector('#image').value.trim();
+    const category = document.querySelector('.form-select').value.trim();
+
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+        const response = await fetch(`/api/products/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ product_name, description, price, image, category }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
         document.location.replace('/dashboard');
       } else {
-        alert("Please check sign up details");
+        alert("Error");
       }
     }
   };
   
 document
-.querySelector('#edit-btn')
+.querySelector('#update-btn')
 .addEventListener('submit', editListingHandler);

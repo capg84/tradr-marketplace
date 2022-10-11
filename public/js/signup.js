d7864c00706f68754f65cf147d248bc1d29ad5d5
P Luong <p-luong@live.co.uk>
2022-10-11 20:15:10 +0100

    amended query selectors

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,16 +2,16 @@ const signupFormHandler = async (event) => {
     console.log("Sign up button clicked");
     event.preventDefault();
 
-    const fName = document.querySelector('#inputFName').value.trim();
-    const lName = document.querySelector('#inputLName').value.trim();
+    const first_name = document.querySelector('#inputFName').value.trim();
+    const last_name = document.querySelector('#inputLName').value.trim();
     const email = document.querySelector('#inputEmail2').value.trim();
     const password = document.querySelector('#inputPassword2').value.trim();
   
-    if (fName && lName && email && password) {
-        console.log(fName + LName + email + password);
-      const response = await fetch('/api/users', {
+    if (first_name && last_name && email && password) {
+        console.log(fName + lName + email + password);
+        const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ fName, lName, email, password }),
+        body: JSON.stringify({ email, password, first_name, last_name }),
         headers: { 'Content-Type': 'application/json' },
       });
   

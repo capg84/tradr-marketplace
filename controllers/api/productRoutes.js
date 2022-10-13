3e42c87528284fc259b57b1c183b9a6dbf783efa
P Luong <p-luong@live.co.uk>
2022-10-13 19:27:36 +0100

    added require statements to productRoutes

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const {Product} = require('../models');
+const withAuth = require('../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
     try {
@@ -7,7 +9,7 @@ router.post('/', withAuth, async (req, res) => {
         user_id: req.session.user_id,
       });
   
-      res.status(200).json(newPost);
+      res.status(200).json(newProduct);
     } catch (err) {
       res.status(400).json(err);
     }

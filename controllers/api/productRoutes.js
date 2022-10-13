482b4bbf07c42e8cc0203d5cbd7fbcc5557dd7ab
P Luong <p-luong@live.co.uk>
2022-10-13 19:26:54 +0100

    added post route to productRoutes

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newProduct = await Product.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newPost);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
 module.exports = router;

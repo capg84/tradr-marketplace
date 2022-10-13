e08e6249ba1a6dca0ab0469c6646ec6bb3a0df47
P Luong <p-luong@live.co.uk>
2022-10-13 20:32:08 +0100

    updated delete route

--- public/js/delete.js
+++ public/js/delete.js
@@ -14,4 +14,4 @@ const delButtonHandler = async (event) => {
     }
 };
 
-document.querySelector('delete-btn').addEventListener('click', delButtonHandler);
+document.querySelector('.delete-btn').addEventListener('click', delButtonHandler);

be3f40dc2ccba66b6783d8bea80e543f497b2b33
P Luong <p-luong@live.co.uk>
2022-10-14 20:58:44 +0100

    installed chart.js

---
+++ public/js/stats.js
@@ -0,0 +1,31 @@
+const Chart = require('chart.js');
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
+    const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
